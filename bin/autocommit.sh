34087ada85060926ad39b57ea4ec0e08113f3562
Stuart P. Bentley <stuart@testtrack4.com>
2018-01-18 05:03:26 +0000

    Vendor in git-slum for autocommitting

--- bin/autocommit.sh
+++ bin/autocommit.sh
@@ -4,7 +4,9 @@ TIMER=10
 
 autocommit () {
   if ! git diff-index --quiet HEAD -- ; then
-    git add -A && git commit -m 'autocommit' && git push -f
+    git add -A &&
+      git commit -m "$($(dirname "$0")/../vendor/git-slum --staged)" &&
+      git push -f
   fi
 }
 
