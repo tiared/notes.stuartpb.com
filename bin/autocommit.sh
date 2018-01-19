5f610698b1aed8b4ec2a821e8a285181aafd858b
Stuart P. Bentley <stuart@testtrack4.com>
2018-01-19 05:53:26 +0000

    Only autocommit on pause in changes

--- bin/autocommit.sh
+++ bin/autocommit.sh
@@ -3,11 +3,22 @@ set -e
 
 TIMER=10
 
+roll_index () {
+  # while there are unstaged changes
+  while git status --porcelain | grep -q '^.[^ ]'; do
+    echo -n '.'
+    git add -A
+    sleep "$TIMER"
+  done
+}
+
 autocommit () {
   dirt=$(git ls-files -dmo --exclude-standard)
   if [[ -n "$dirt" ]]; then
-    git add -A &&
-      git commit -m "$($(dirname "$0")/../vendor/git-slum --staged)" &&
+    echo -n 'Building commit..'
+    roll_index
+    echo
+    git commit -m "$($(dirname "$0")/../vendor/git-slum --staged)" &&
       git push -f
   fi
 }
