e7850b957250a4fa1c7ec40cdd9ce2476a1d8af9
Stuart P. Bentley <stuart@testtrack4.com>
2018-01-19 10:10:49 +0000

    Autocommit immediately at the beginning of autocommit.sh
    This is to facilitate the duality of autocommits described in docs/hemingway-style.md

--- bin/autocommit.sh
+++ bin/autocommit.sh
@@ -6,8 +6,14 @@ POLL=1
 # How many polls to wait for changes to settle before cutting a commit.
 FUSETIME=10
 
+autocommit () {
+  git commit -m "$($(dirname "$0")/../vendor/git-slum --staged)" &&
+    git push -f
+}
+
 roll_index () {
   local SHOTCLOCK
+  echo
   while (( SHOTCLOCK <= FUSETIME )); do
     # reset the shotclock whenever there are unstaged changes
     if git status --porcelain | grep -q '^.[^ ]'; then
@@ -26,17 +32,15 @@ roll_index () {
     SHOTCLOCK=$((SHOTCLOCK + 1))
     if ((SHOTCLOCK <= FUSETIME)); then sleep "$POLL"; fi
   done
+  echo
 }
 
-autocommit () {
+# this checks if there's any change from the working tree to HEAD
+# this can be true even when all those changes are staged
+# that's the main distinction between this check and the one in roll_index
+check_dirty () {
   dirt=$(git ls-files -dmo --exclude-standard)
-  if [[ -n "$dirt" ]]; then
-    echo
-    roll_index
-    echo
-    git commit -m "$($(dirname "$0")/../vendor/git-slum --staged)" &&
-      git push -f
-  fi
+  [[ -n "$dirt" ]]
 }
 
 DANCESTEP=0
@@ -44,9 +48,21 @@ DANCEMOVES=(
   $'\e[1;93m'"^('-')^"$'\e[0m'
   $'\e[1;93m'"v('-')v"$'\e[0m')
 
-autocommit
-while sleep "$POLL"; do
-  echo -n $'\r'"Watching for changes... ${DANCEMOVES[DANCESTEP]} "
-  DANCESTEP=$(( (DANCESTEP + 1) % ${#DANCEMOVES[@]} ))
+watch_for_changes () {
+  while :; do
+    echo -n $'\r'"Watching for changes... ${DANCEMOVES[DANCESTEP]} "
+    DANCESTEP=$(( (DANCESTEP + 1) % ${#DANCEMOVES[@]} ))
+    if check_dirty; then
+      roll_index
+      autocommit
+    fi
+    sleep "$POLL"
+  done
+}
+
+
+if check_dirty; then
+  git add -A
   autocommit
-done
+fi
+watch_for_changes
