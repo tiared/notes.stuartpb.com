1b114f99778f2caa822122b83dea6a7e362f64d8
Stuart P. Bentley <stuart@testtrack4.com>
2018-01-19 06:33:44 +0000

    Animate while waiting for changes with little yellow dancing dude

--- bin/autocommit.sh
+++ bin/autocommit.sh
@@ -1,20 +1,26 @@
 #! /usr/bin/env bash
 set -e
 
-TIMER=10
+# Times to wait.
+
+# How frequently to check if the working tree has changed.
+POLL=1
+# How long to wait for changes to settle before cutting a commit.
+SETTLE=5
 
 roll_index () {
   # while there are unstaged changes
   while git status --porcelain | grep -q '^.[^ ]'; do
     echo -n '.'
     git add -A
-    sleep "$TIMER"
+    sleep "$SETTLE"
   done
 }
 
 autocommit () {
   dirt=$(git ls-files -dmo --exclude-standard)
   if [[ -n "$dirt" ]]; then
+    echo
     echo -n 'Building commit..'
     roll_index
     echo
@@ -23,5 +29,14 @@ autocommit () {
   fi
 }
 
+DANCESTEP=0
+DANCEMOVES=(
+  $'\e[1;93m'"(>'-')>"$'\e[0m'
+  $'\e[1;93m'"<('-'<)"$'\e[0m')
+
 autocommit
-while sleep "$TIMER"; do autocommit; done
+while sleep "$POLL"; do
+  echo -n $'\r'"Watching for changes... ${DANCEMOVES[DANCESTEP]} "
+  DANCESTEP=$(((DANCESTEP + 1) % ${#DANCEMOVES[@]}))
+  autocommit
+done
